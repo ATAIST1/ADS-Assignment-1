86d1efa4f5933543407c228de0ddf47a82573d8f
Ернар <230709@astanait.edu.kz>
2024-04-10 01:44:40 +0500

    8

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,42 +1,34 @@
 package org.example;
+import java.util.Scanner;
+
+
 public class Main {
     public static void main(String[] args) {
-        printSpiralSquare(3);
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int k = scanner.nextInt();
+        scanner.close();
+        int[] sequence = new int[n];
+        generateSequences(sequence, 0, k);
+    }
+
+
+    public static void generateSequences(int[] sequence, int index, int k) {
+        if (index == sequence.length) {
+            printArray(sequence);
+            return;
+        }
+
+
+        for (int i = 1; i <= k; i++) {
+            sequence[index] = i;
+            generateSequences(sequence, index + 1, k);
+        }
+    }
+    public static void printArray(int[] arr) {
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
         System.out.println();
-        printSpiralSquare(4);
-    }
-    public static void printSpiralSquare(int n) {
-        int[][] matrix = new int[n][n];
-        fillMatrix(matrix, 0, n - 1, 1);
-        printMatrix(matrix);
-    }
-    public static void fillMatrix(int[][] matrix, int start, int end, int num) {
-        if (start > end) return;
-        for (int i = start; i <= end; i++) {
-            matrix[start][i] = num++;
-        }
-
-        for (int i = start + 1; i <= end; i++) {
-            matrix[i][end] = num++;
-        }
-
-        for (int i = end - 1; i >= start; i--) {
-            matrix[end][i] = num++;
-        }
-
-        for (int i = end - 1; i > start; i--) {
-            matrix[i][start] = num++;
-        }
-        fillMatrix(matrix, start + 1, end - 1, num);
-    }
-
-
-    public static void printMatrix(int[][] matrix) {
-        for (int[] row : matrix) {
-            for (int cell : row) {
-                System.out.print(cell + " ");
-            }
-            System.out.println();
-        }
     }
 }
