d514642352040b4d1587919ccc5b173c6f23a575
Ернар <230709@astanait.edu.kz>
2024-04-10 01:39:33 +0500

    4

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,17 @@
 package org.example;
 
 public class Main {
-    public static int sumToN(int n) {
-        if(n == 1) {
+    public static double sumToN(double b,double n) {
+        if(n == 0) {
             return 1;
         }
         else {
-            return n + sumToN(n-1);
+            return Math.pow(b, n) + sumToN(b, n-1);
         }
     }
 
 
     public static void main(String[] args) {
-        System.out.println(sumToN(5));
+        System.out.println(sumToN(3,3));
     }
 }
