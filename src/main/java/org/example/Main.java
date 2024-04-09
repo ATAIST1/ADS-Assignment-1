5f995de5f1091f2cebbfdfd72531338697dc5446
Ернар <230709@astanait.edu.kz>
2024-04-10 01:36:53 +0500

    1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,4 @@
 package org.example;
-
-
-
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static int squareSum(int n) {
         if(n == 1) {
@@ -17,8 +11,6 @@ public class Main {
 
 
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
         System.out.println(squareSum(5));
     }
 }
