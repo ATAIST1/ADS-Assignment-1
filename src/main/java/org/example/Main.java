7d253f3ed656cf48e9804376312b7c76f68c1a6b
Ернар <230709@astanait.edu.kz>
2024-04-10 01:44:05 +0500

    7

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,25 +1,42 @@
 package org.example;
-
-
-import java.util.Scanner;
-
-
 public class Main {
-
-
-    static Scanner scanner = new Scanner(System.in);
-    static void reverseSequence(int n) {
-        if (n == 0) {
-            return;
+    public static void main(String[] args) {
+        printSpiralSquare(3);
+        System.out.println();
+        printSpiralSquare(4);
+    }
+    public static void printSpiralSquare(int n) {
+        int[][] matrix = new int[n][n];
+        fillMatrix(matrix, 0, n - 1, 1);
+        printMatrix(matrix);
+    }
+    public static void fillMatrix(int[][] matrix, int start, int end, int num) {
+        if (start > end) return;
+        for (int i = start; i <= end; i++) {
+            matrix[start][i] = num++;
         }
-        String string = scanner.nextLine();
-        reverseSequence(n - 1);
-        System.out.println(string);
+
+        for (int i = start + 1; i <= end; i++) {
+            matrix[i][end] = num++;
+        }
+
+        for (int i = end - 1; i >= start; i--) {
+            matrix[end][i] = num++;
+        }
+
+        for (int i = end - 1; i > start; i--) {
+            matrix[i][start] = num++;
+        }
+        fillMatrix(matrix, start + 1, end - 1, num);
     }
 
 
-    public static void main(String[] args) {
-        int n = scanner.nextInt();
-        reverseSequence(n);
+    public static void printMatrix(int[][] matrix) {
+        for (int[] row : matrix) {
+            for (int cell : row) {
+                System.out.print(cell + " ");
+            }
+            System.out.println();
+        }
     }
 }
