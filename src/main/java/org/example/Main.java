1458afb2f97d2d437d6fb178fe6834082b8d20ef
Ернар <230709@astanait.edu.kz>
2024-04-10 01:41:50 +0500

    5

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,25 +1,25 @@
 package org.example;
 
+
 import java.util.Scanner;
-import java.util.ArrayList;
+
+
 public class Main {
-    static Scanner sc = new Scanner(System.in);
-    public static String inputSum(int n, ArrayList<Integer> list){
-        if(n == 1) {
-            int b = sc.nextInt();
-            return b+ "";
+
+
+    static Scanner scanner = new Scanner(System.in);
+    static void reverseSequence(int n) {
+        if (n == 0) {
+            return;
         }
-        else {
-            int b = sc.nextInt();
-            list.add(b);
-            return inputSum(n-1, list) + " " + b;
-        }
+        String string = scanner.nextLine();
+        reverseSequence(n - 1);
+        System.out.println(string);
     }
 
 
     public static void main(String[] args) {
-        int r = sc.nextInt();
-        ArrayList<Integer> list = new ArrayList<>();
-        System.out.println(inputSum(r, list));
+        int n = scanner.nextInt();
+        reverseSequence(n);
     }
 }
