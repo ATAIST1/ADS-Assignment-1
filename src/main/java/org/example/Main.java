89e49a5fa12c5ef9ffb64c2f5cf3a83e8ac6c7d0
Ернар <230709@astanait.edu.kz>
2024-04-10 01:45:28 +0500

    9

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,34 +1,31 @@
 package org.example;
-import java.util.Scanner;
+import java.util.Arrays;
 
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        int k = scanner.nextInt();
-        scanner.close();
-        int[] sequence = new int[n];
-        generateSequences(sequence, 0, k);
+        String input1 = "AB";;
+        printPermutations(input1.toCharArray(), 0);
+
+
+        String input2 = "IOX";
+        printPermutations(input2.toCharArray(), 0);
     }
-
-
-    public static void generateSequences(int[] sequence, int index, int k) {
-        if (index == sequence.length) {
-            printArray(sequence);
+    public static void printPermutations(char[] chars, int currentIndex) {
+        if (currentIndex == chars.length - 1) {
+            System.out.println(String.valueOf(chars));
             return;
         }
 
-
-        for (int i = 1; i <= k; i++) {
-            sequence[index] = i;
-            generateSequences(sequence, index + 1, k);
+        for (int i = currentIndex; i < chars.length; i++) {
+            swap(chars, currentIndex, i);
+            printPermutations(chars, currentIndex + 1);
+            swap(chars, currentIndex, i);
         }
     }
-    public static void printArray(int[] arr) {
-        for (int num : arr) {
-            System.out.print(num + " ");
-        }
-        System.out.println();
+    public static void swap(char[] chars, int i, int j) {
+        char temp = chars[i];
+        chars[i] = chars[j];
+        chars[j] = temp;
     }
 }
