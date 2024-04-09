234e425f752ae7b842235f8de0a909eab88d3560
Ернар <230709@astanait.edu.kz>
2024-04-10 01:45:53 +0500

    10

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,31 +1,17 @@
 package org.example;
-import java.util.Arrays;
-
-
 public class Main {
+    public static boolean isPowerOfTwo(int n) {
+        if (n <= 0) {
+            return false;
+        }
+        while (n % 2 == 0) {
+            n /= 2;
+        }
+        return n == 1;
+    }
+
+
     public static void main(String[] args) {
-        String input1 = "AB";;
-        printPermutations(input1.toCharArray(), 0);
-
-
-        String input2 = "IOX";
-        printPermutations(input2.toCharArray(), 0);
-    }
-    public static void printPermutations(char[] chars, int currentIndex) {
-        if (currentIndex == chars.length - 1) {
-            System.out.println(String.valueOf(chars));
-            return;
-        }
-
-        for (int i = currentIndex; i < chars.length; i++) {
-            swap(chars, currentIndex, i);
-            printPermutations(chars, currentIndex + 1);
-            swap(chars, currentIndex, i);
-        }
-    }
-    public static void swap(char[] chars, int i, int j) {
-        char temp = chars[i];
-        chars[i] = chars[j];
-        chars[j] = temp;
+        System.out.println(isPowerOfTwo(16));
     }
 }
